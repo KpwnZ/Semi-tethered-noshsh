dfda8797658d3c7b55d777c0837fb1ff4c516b35
XCXiao <958169971@qq.com>
2016-10-22 21:51:26 +0800

    Create Daemon.m

---
+++ Daemon.m
@@ -0,0 +1,10 @@
+static void loadLLB(CFNotificationCenterRef center, void *observer, CFStringRef name, const     void *object, CFDictionaryRef userInfo)
+{
+	system("magic");//kloader LLB.img3
+}
+int main(int argc, char **argv, char **envp)
+{
+	CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, loadLLB, CFSTR("com.kpwn0.loadllb"), NULL,     CFNotificationSuspensionBehaviorCoalesce);
+	CFRunLoopRun();
+	return 0;
+}
